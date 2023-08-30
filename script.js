61359d8fb25d6be1f3f03008acc232eb19aa7e65
KRainbolt21 <139176388+KRainbolt21@users.noreply.github.com>
2023-08-30 16:59:13 -0700

    Create function to determine winner of each round and display a message of the outcome

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function getComputerChoice(answer) {
 }
 
 let playerSelection = prompt("Choose between rock, paper, or scissors", "");
+let computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == "rock" && computerSelection == "Rock") {
+        alert ("Tie! Please play again.");
+    } else if (playerSelection == "paper" && computerSelection == "Rock") {
+        alert ("You win! Paper beats rock.");
+    }
+}
