5e3696d810c00f89c49f58d05b5be5bacf0639ed
KRainbolt21 <139176388+KRainbolt21@users.noreply.github.com>
2023-08-30 16:28:29 -0700

    Use math.random to return random item from answer array

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function getComputerChoice(answer) {
     const answer = ["Rock", "Paper", "Scissors"];
+    return answer[Math.floor((Math.random()*answer.length))];
 }
+
+console.log(getComputerChoice());
