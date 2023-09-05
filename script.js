78e41a13d261e5840e264538827bcc04e5bfc79d
KRainbolt21 <139176388+KRainbolt21@users.noreply.github.com>
2023-09-05 16:34:48 -0700

    Create game function to keep score each round

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function playRound(playerSelection, computerSelection) {
     if(playerSelection == "paper" && computerSelection == "Scissors") {
         alert("You lose. Scissors beat paper");
     } else if (playerSelection == "paper" && computerSelection == "Rock") {
-        alert("You win! Paper beats rock.");
+        alert("You win! Paper beats rock");
     } else if (playerSelection == "scissors" && computerSelection == "Rock") {
         alert("You lose. Rock beats scissors");
     } else if (playerSelection == "rock" && computerSelection == "Scissors") {
@@ -22,5 +22,16 @@ function playRound(playerSelection, computerSelection) {
         alert("You win! Scissors beat paper");
     } else {
         alert("Tie! Please try again");
+    }
+}
+
+let computerScore = 0;
+let playerScore = 0;
+
+function game(playRound) {
+  if(alert == "You lose. Scissors beat paper" || alert == "You lose. Rock beats scissors" || alert == "You lose. Paper beats rock") {
+       return computerScore++;
+    } else if(alert == "You win! Paper beats rock" || alert == "You win! rock beats scissors" || alert == "You win! Scissors beat paper") {
+        return playerScore++;
     }
 }
