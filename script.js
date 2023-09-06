98fcbd2da46cd588246a6673f9be62c464020a0e
KRainbolt21 <139176388+KRainbolt21@users.noreply.github.com>
2023-09-06 05:37:24 -0700

    Rearrange playRound function to be more concise

--- script.js
+++ script.js
@@ -8,34 +8,15 @@ let computerSelection = getComputerChoice();
 let playerSelection = playerChoice.toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection == "paper" && computerSelection == "Scissors") {
+    if(playerSelection == "paper" && computerSelection == "Scissors" || playerSelection == "scissors" && computerSelection == "Rock" ||playerSelection == "rock" && computerSelection == "Paper") {
         return "You lose.";
-    } else if (playerSelection == "paper" && computerSelection == "Rock") {
-        return "You win!";
-    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
-        return "You lose.";
-    } else if (playerSelection == "rock" && computerSelection == "Scissors") {
-        return "You win!";
-    } else if (playerSelection == "rock" && computerSelection == "Paper") {
-        return "You lose.";
-    } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+    } else if (playerSelection == "paper" && computerSelection == "Rock" || playerSelection == "rock" && computerSelection == "Scissors" || layerSelection == "scissors" && computerSelection == "Paper") {
         return "You win!";
     } else {
         return "It's a tie!";
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
-
 const outcome = playRound(playerSelection, computerSelection);
-
-function game(outcome, computerScore, playerScore) {
-  if(outcome == "You lose.") {
-       return ++computerScore;
-    } else if(outcome == "You win!") {
-        return ++playerScore;
-    }
-}
-
-let computerScore = 0;
 let playerScore = 0;
+let computerScore = 0;
